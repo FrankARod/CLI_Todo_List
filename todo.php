78415f1910509724f5de73a815bb379d5ed8cf31
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-20 09:37:07 -0500

    Added sorting feature

--- todo.php
+++ todo.php
@@ -16,10 +16,9 @@
             $list_to_print = "Empty TODO List" . PHP_EOL;
     } else {
         foreach ($list as $key => $item) {
-            $list_to_print .= "[" . ++$key . "] $item" . PHP_EOL;
+            $list_to_print .= "[" . ++$key . "]\t$item" . PHP_EOL;
         }
     }
-    
     return $list_to_print;
  }
 
@@ -29,13 +28,31 @@
    return ($menu_input ? substr(strtoupper(trim(fgets(STDIN))), 0, 1) : trim(fgets(STDIN)));  
  }
 
+function sort_menu($list){
+    switch(get_input(TRUE)) {
+        case 'A':
+            asort($list);
+            break;
+        case 'Z':
+            arsort($list);
+            break;
+        case 'O':
+            ksort($list);
+            break;
+        case 'R':
+            krsort($list);
+            break;
+    }
+    return $list;
+}
+
  // The loop!
  do {
      // Echo the list produced by the function
      echo list_items($items);
 
      // Show the menu options
-     echo '(N)ew item, (R)emove item, (Q)uit : ';
+     echo '(N)ew item, (S)ort, (R)emove item, (Q)uit : ';
 
      // Get the input from user
      // Use trim() to remove whitespace and newlines
@@ -52,15 +69,19 @@
          echo 'Enter item number to remove: ';
          // Get array key
          $key = get_input();
-         $key--;
          // Remove from array
-         unset($items[$key]);
+         unset($items[--$key]);
+     } elseif($input == 'S') {
+        echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered" . PHP_EOL;
+        $items = sort_menu($items);
+     } else {
+        echo 'Invalid input' . PHP_EOL;
      }
  // Exit when input is (Q)uit
  } while ($input != 'Q');
 
  // Say Goodbye!
- echo "Goodbye!\n";
+ echo "Goodbye!" . PHP_EOL;
 
  // Exit with 0 errors
  exit(0);
