53c8e3c747a286dea67fa82d5330c1a529df51ed
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-09-15 11:13:52 -0500

    Converted spaces to tabs

--- todo.php
+++ todo.php
@@ -6,20 +6,20 @@ $items = array();
 // List array items formatted for CLI
 function list_items($list)
 {
-    // Return string of list items separated by newlines.
-    // Should be listed [KEY] Value like this:
-    // [1] TODO item 1
-    // [2] TODO item 2 - blah
-    // DO NOT USE ECHO, USE RETURN
-    $list_to_print = "";
-    if (empty($list)) {
-            $list_to_print = "Empty TODO List" . PHP_EOL;
-    } else {
-        foreach ($list as $key => $item) {
-            $list_to_print .= "[" . ++$key . "]\t$item" . PHP_EOL;
-        }
-    }
-    return $list_to_print;
+	// Return string of list items separated by newlines.
+	// Should be listed [KEY] Value like this:
+	// [1] TODO item 1
+	// [2] TODO item 2 - blah
+	// DO NOT USE ECHO, USE RETURN
+	$list_to_print = "";
+	if (empty($list)) {
+			$list_to_print = "Empty TODO List" . PHP_EOL;
+	} else {
+		foreach ($list as $key => $item) {
+			$list_to_print .= "[" . ++$key . "]\t$item" . PHP_EOL;
+		}
+	}
+	return $list_to_print;
 }
 
 // Get STDIN, strip whitespace and newlines, 
@@ -29,131 +29,125 @@ function get_input($menu_input = FALSE) {
  }
 
 function sort_menu($list){
-    switch(get_input(TRUE)) {
-        case 'A':
-            asort($list);
-            break;
-        case 'Z':
-            arsort($list);
-            break;
-        case 'O':
-            ksort($list);
-            break;
-        case 'R':
-            krsort($list);
-            break;
-    }
-    return $list;
+	switch(get_input(TRUE)) {
+		case 'A':
+			asort($list);
+			break;
+		case 'Z':
+			arsort($list);
+			break;
+		case 'O':
+			ksort($list);
+			break;
+		case 'R':
+			krsort($list);
+			break;
+	}
+	return $list;
 }
 
 function add_file($filepath, $old_list) {
-    /*$handle = fopen($filepath, 'r');
-    $saved_list = trim(fread($handle, filesize($filepath))); */
-
-    $new_list = file($filepath, FILE_IGNORE_NEW_LINES);
-    return array_merge($old_list, $new_list);
+	$new_list = file($filepath, FILE_IGNORE_NEW_LINES);
+	return array_merge($old_list, $new_list);
 }
 
 function create_file($filename, $items) {
-    if (file_exists($filename)) {
-        echo "$filename exists! Overwrite?" . PHP_EOL . "(Y)es or (N)o" . PHP_EOL;
-        $input = get_input(true);
-        switch($input) {
-            case "Y":
-                break;
-            case "N":
-                return "Save Canceled";
-        }
-    }
-    $items = implode("\n", $items);
-    $handle = fopen($filename, 'w');
-    fwrite($handle, $items);
-    fclose($handle);
-    return "Save Successful";
+	if (file_exists($filename)) {
+		echo "$filename exists! Overwrite?" . PHP_EOL . "(Y)es or (N)o" . PHP_EOL;
+		$input = get_input(true);
+		switch($input) {
+			case "Y":
+				break;
+			case "N":
+				return "Save Canceled";
+		}
+	}
+	$items = implode("\n", $items);
+	$handle = fopen($filename, 'w');
+	fwrite($handle, $items);
+	fclose($handle);
+	return "Save Successful";
 }
 
 // The loop!
 do {
-    // Echo the list produced by the function
-    echo list_items($items);
+	// Echo the list produced by the function
+	echo list_items($items);
 
-    // Show the menu options
-    echo '(N)ew item, (O)pen, s(A)ve, (S)ort, (R)emove item, (Q)uit : ';
+	// Show the menu options
+	echo '(N)ew item, (O)pen, s(A)ve, (S)ort, (R)emove item, (Q)uit : ';
 
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = get_input(TRUE);
+	// Get the input from user
+	// Use trim() to remove whitespace and newlines
+	$input = get_input(TRUE);
 
-    // Check for actionable input
+	// Check for actionable input
 
-    switch($input) {
-        case 'N':
-            // Ask for entry
-            echo 'Enter item: ';
-            // Add entry to list array
-            $new_item = get_input();
-            echo "Place at (B)eginning or (E)nd of List? ";
-            switch(get_input(TRUE)) {
-                case 'B':
-                    array_unshift($items, $new_item);
-                    break;
-                case 'E': 
-                    array_push($items, $new_item);
-                    break;
-                default:
-                    array_push($items, $new_item);
-                    break;
-            }
-            break;
-        
-        case 'R':
-            // Remove which item?
-            echo 'Enter item number to remove: ';
-            // Get array key
-            $key = get_input();
-            // Remove from array
-            unset($items[--$key]);
-            break;
+	switch($input) {
+		case 'N':
+			// Ask for entry
+			echo 'Enter item: ';
+			// Add entry to list array
+			$new_item = get_input();
+			echo "Place at (B)eginning or (E)nd of List? ";
+			switch(get_input(TRUE)) {
+				case 'B':
+					array_unshift($items, $new_item);
+					break;
+				case 'E': 
+					array_push($items, $new_item);
+					break;
+				default:
+					array_push($items, $new_item);
+					break;
+			}
+			break;
+		
+		case 'R':
+			// Remove which item?
+			echo 'Enter item number to remove: ';
+			// Get array key
+			$key = get_input();
+			// Remove from array
+			unset($items[--$key]);
+			break;
 
-        case 'S':
-            echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered" . PHP_EOL;
-            $items = sort_menu($items);
-            break;
+		case 'S':
+			echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered" . PHP_EOL;
+			$items = sort_menu($items);
+			break;
 
-        case 'F':
-            array_shift($items);
-        break;
+		case 'F':
+			array_shift($items);
+			break;
 
-        case 'L':
-            array_pop($items);
-        break;
+		case 'L':
+			array_pop($items);
+			break;
 
-        case 'O':
-            echo "Enter a file to add to the list" . PHP_EOL;
-            $filepath = get_input();
-            $items = add_file($filepath, $items);
-            break;
+		case 'O':
+			echo "Enter a file to add to the list" . PHP_EOL;
+			$filepath = get_input();
+			$items = add_file($filepath, $items);
+			break;
 
-        case 'A':
-            echo "Enter a filename for the list" . PHP_EOL;
-            $filename = get_input();
-            echo create_file($filename, $items) . PHP_EOL;
-            break;
+		case 'A':
+			echo "Enter a filename for the list" . PHP_EOL;
+			$filename = get_input();
+			echo create_file($filename, $items) . PHP_EOL;
+			break;
 
-        case 'Q':
-             // Say Goodbye!
-            echo "Goodbye!" . PHP_EOL;
-            break;    
+		case 'Q':
+			 // Say Goodbye!
+			echo "Goodbye!" . PHP_EOL;
+			break;    
 
-        default: 
-            echo 'Invalid input' . PHP_EOL;
-            break;
-    }
+		default: 
+			echo 'Invalid input' . PHP_EOL;
+			break;
+	}
  // Exit when input is (Q)uit
 } while ($input != 'Q');
-
-
-
 
 // Exit with 0 errors
 exit(0);
